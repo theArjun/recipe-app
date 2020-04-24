d755015352c7c72853dd9015fe92dd15bd272d2d
thearjun <mailarjunadhikari@gmail.com>
2020-04-25 03:08:50 +0545

    Updates

---
+++ precache-manifest.f4c45b11e5de2925fefb2e900a59d8ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b6b98e82d70b5df9c8a6cb1172ff64",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "c814390184dd47d9ff9f",
+    "url": "/recipe-app/static/css/2.1174ccc1.chunk.css"
+  },
+  {
+    "revision": "57122ea6221499dfa9b2",
+    "url": "/recipe-app/static/css/main.ff6c3df4.chunk.css"
+  },
+  {
+    "revision": "c814390184dd47d9ff9f",
+    "url": "/recipe-app/static/js/2.163821d5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/recipe-app/static/js/2.163821d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57122ea6221499dfa9b2",
+    "url": "/recipe-app/static/js/main.20d3558f.chunk.js"
+  },
+  {
+    "revision": "6c108a12729393597590",
+    "url": "/recipe-app/static/js/runtime-main.2b464d9d.js"
+  }
+]);
